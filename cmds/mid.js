2a8f8736950b5b8207f9173640f64f100cfc6f06
Glitch (maize-traih-l-partridgegchos) <none>
2021-08-31 05:38:17 +0000

    Exporting to hungcho99/SpermBot ./cmds/mid.js:24551704/2470

--- cmds/mid.js
+++ cmds/mid.js
@@ -6,7 +6,7 @@ var __importDefault =
   };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.location = exports.threadAdminRequired = exports.adminRequired = exports.name = void 0;
-exports.name = "mid";
+exports.name = "fes";
 exports.adminRequired = false;
 exports.threadAdminRequired = false;
 exports.location = __filename;
@@ -17,7 +17,16 @@ async function default_1({ event, api, botID, getThread }) {
     const minutes = Math.floor((ms/1000/60)%60);
     const hours = Math.floor((ms/(1000*60*60))%24);
     const days = Math.floor(ms/(1000*60*60*24));
-    return api.sendMessage(`Sắp đến TẾT TRUNG THU 2021 rồi! Chỉ còn...\n${days} ngày ${hours} tiếng ${minutes} phút ${seconds} giây.`, event.threadID, event.messageID);
+   
+    const mss = Date.parse("Jan 31, 2022 17:00:00") - Date.parse(new Date());
+    const secondss = Math.floor((mss/1000)%60);
+    const minutess = Math.floor((mss/1000/60)%60);
+    const hourss = Math.floor((mss/(1000*60*60))%24);
+    const dayss = Math.floor(mss/(1000*60*60*24));
+   
+
+    return api.sendMessage(`Sắp đến TẾT TRUNG THU 2021 rồi! Chỉ còn...\n${days} ngày ${hours} tiếng ${minutes} phút ${seconds} giây.\n______________________________\nTẾT NHÂM DẦN(2022) cách chúng ta xa lắm...\n${dayss} ngày ${hourss} tiếng ${minutess} phút ${secondss} giây.`, event.threadID, event.messageID);
+
 }
   
 exports.default = default_1;
