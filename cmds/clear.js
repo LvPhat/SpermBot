a3140db932c6cbf6551c28fe58a851b21b7b506a
Glitch (maize-traih-l-partridgegchos) <none>
2021-09-08 02:56:21 +0000

    ⛈❄️ Checkpoint ./cmds/clear.js:24551704/187

--- cmds/clear.js
+++ cmds/clear.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.location = exports.threadAdminRequired = exports.adminRequired = exports.name = void 0;
 const fs_extra_1 = require("fs-extra");
-exports.name = "clear";
+exports.name = "stas";
 exports.adminRequired = true;
 exports.threadAdminRequired = false;
 exports.location = __filename;
@@ -10,6 +10,7 @@ async function default_1({ event, botData, api, getThread }) {
   const disk = require("diskusage");
   const { free } = await disk.check('/app');
   const diskF = free / 1024
-  api.sendMessage(diskF.toLocaleString('vi-VN') + "MB", event.threadID)
+  var msg = 
+  api.sendMessage( + "MB", event.threadID)
 }
 exports.default = default_1;
