d71b3cf9a5a90d25050ad2d4f86addb705d70942
Glitch (maize-traih-l-partridgegchos) <none>
2021-09-09 03:02:40 +0000

    🚥🏘 Checkpoint ./cmds/covid.js:24551704/2487

--- cmds/covid.js
+++ cmds/covid.js
@@ -10,21 +10,39 @@ exports.adminRequired = false;
 exports.threadAdminRequired = false;
 exports.location = __filename;
 async function default_1({ event, api }) {
-    try {
-        let { data } = await axios_1.default.get('https://www.spermlord.ga/covid');
-        api.sendMessage('====== Thế Giới ======\n' +
-            `😷 Nhiễm: ${data.thegioi.nhiem}\n` +
-            `💚 Hồi phục: ${data.thegioi.hoiphuc}\n` +
-            `💀 Tử vong: ${data.thegioi.tuvong}\n` +
-            '====== Việt Nam ======\n' +
-            `😷 Nhiễm: ${data.vietnam.nhiem}\n` +
-            `💚 Hồi phục: ${data.vietnam.hoiphuc}\n` +
-            `💀 Tử vong: ${data.vietnam.tuvong}\n\n` +
-            `📰 Tin tức: ${data.tintuc}.\n` +
-            `⌚ Cập nhật: ${data.updatedAt}.`, event.threadID, event.messageID);
-    }
-    catch {
-        return api.sendMessage('Đã có lỗi xảy ra.', event.threadID, event.messageID);
-    }
+  var { data } = await axios_1.get("https://meewmeew.info/covid?apikey=Meew_UwbZldigKMeCrqWMiw7UbIPDOfDqOB");
+    if (data.success == false) return api.sendMessage(data.error, event.threadID);
+    var world = data.world,
+        vn = data.vietnam,
+        news = data.news,
+        nhiemtg = world.cases,
+        chettg = world.deaths,
+        hoiphuctg = world.recovered,
+        nhiemvn = vn.cases,
+        chetvn = vn.deaths,
+        hoiphucvn = vn.recovered,
+        dieutrivn = vn.recovering,      
+        ptchetvn = Math.round(chetvn.replace(/\./g,"") * 100 / nhiemvn.replace(/\./g,"")),
+        pthoiphucvn = Math.round(hoiphucvn.replace(/\./g,"") * 100 / nhiemvn.replace(/\./g,"")),
+        ptchettg = Math.round(chettg.replace(/\./g,"") * 100 / nhiemtg.replace(/\./g,"")),
+        pthoiphuctg = Math.round(hoiphuctg.replace(/\./g,"") * 100 / nhiemtg.replace(/\./g,"")),
+        pthoiphucvn = pthoiphucvn.toString().split(".")[0],
+        ptdieutrivn = (100 - pthoiphucvn - ptchetvn).toString().split(".")[0];
+    ptchetvn = ptchetvn.toString().split(".")[0];
+    pthoiphuctg = pthoiphuctg.toString().split(".")[0];
+    ptchettg = ptchettg.toString().split(".")[0];    
+    return api.sendMessage(
+        '====== Thế Giới ======\n' +
+        `😷 Nhiễm: ${nhiemtg}\n` +
+        `💚 Hồi phục: ${hoiphuctg} (${pthoiphuctg}%)\n` +
+        `💀 Tử vong: ${chettg} (${ptchettg}%)\n` +
+        '====== Việt Nam ======\n' +
+        `😷 Nhiễm: ${nhiemvn}\n` +
+        `💉 Đang điều trị: ${dieutrivn} (${ptdieutrivn}%)\n` +
+        `💚 Hồi phục: ${hoiphucvn} (${pthoiphucvn}%)\n` +
+        `💀 Tử vong: ${chetvn} (${ptchetvn}%)\n\n` +
+        `Tin tức: ${news.vietnam}\n` +
+        `Cập nhật: ${data.time}`, event.threadID
+    );
 }
 exports.default = default_1;
