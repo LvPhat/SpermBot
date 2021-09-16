c973be63b0f486b99e185b92e14d68cdaf828ac2
Glitch (faithful-cubic-whippoorwill) <none>
2021-09-16 15:18:07 +0000

    💷👱 Checkpoint ./cmds/test.js:24551704/373

--- cmds/test.js
+++ cmds/test.js
@@ -10,15 +10,20 @@ const axios_1 = __importDefault(require("axios"));
 const axios = require("axios")
 const cheerio_1 = __importDefault(require("cheerio"));
 const fs_extra_1 = require("fs")
-exports.name = "test";
+exports.name = "";
 exports.adminRequired = false;
 exports.threadAdminRequired = false;
 exports.location = __filename;
 async function default_1({ event, api }) {
-  var data =  await fs_extra_1.readFile(`${__dirname}/${event.args[0]}.js`,'utf-8',(err, data) => {
- if(err) console.log(err);
-    api.sendMessage(data, event.threadID)
+  if(event.args[0] == 'read'){
+  var data =  await fs_extra_1.readFile(`${__dirname}/${event.args[1]}.js`,'utf-8',(err, data) => {
+  if(err) return api.sendMessage(`Đã xảy ra lỗi khi đọc lệnh ${event.args[1]}`, event.threadID, event.messageID)
+    api.sendMessage(data, event.threadID, event.messageID)
   })
+ }
+  else if (event.args[0] == 'edit'){
   
+  }
+
 }
 exports.default = default_1;
