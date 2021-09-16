6ababec9cac7b9a2be2c9c4313c9032f42087693
Glitch (faithful-cubic-whippoorwill) <none>
2021-09-16 16:15:23 +0000

    ⏰👯 Checkpoint ./cmds/code.js:24551704/4453

--- cmds/code.js
+++ cmds/code.js
@@ -7,35 +7,64 @@ var __importDefault =
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.location = exports.threadAdminRequired = exports.adminRequired = exports.name = void 0;
 const axios_1 = __importDefault(require("axios"));
-const axios = require("axios")
+const axios = require("axios");
 const cheerio_1 = __importDefault(require("cheerio"));
-const fs_extra_1 = require("fs")
+const fs_extra_1 = require("fs");
+const fs = require("fs-extra");
 exports.name = "code";
 exports.adminRequired = true;
 exports.threadAdminRequired = false;
 exports.location = __filename;
 async function default_1({ event, api }) {
-  if (event.args[0] == 'edit'){
-  var newCode = event.contentMsg.slice((2 + event.args[1].length + event.args[0].length), event.contentMsg.length);
-  console.log(newCode) 
-  fs_extra_1.writeFile(`${__dirname}/${event.args[1]}.js`, newCode, 'utf-8',function(err) {
-    if(err) return api.sendMessage(`Đã Đã xảy ra lỗi khi áp dụng code mới cho "${event.args[1]}.js"`)
-    api.sendMessage(`Đã áp dụng code mới cho "${event.args[1]}.js"`, event.threadID)
-  })
+  if (event.args[0] == "edit") {
+    var newCode = event.contentMsg.slice(
+      2 + event.args[1].length + event.args[0].length,
+      event.contentMsg.length
+    );
+    console.log(newCode);
+    fs_extra_1.writeFile(
+      `${__dirname}/${event.args[1]}.js`,
+      newCode,
+      "utf-8",
+      function(err) {
+        if (err)
+          return api.sendMessage(
+            `Đã Đã xảy ra lỗi khi áp dụng code mới cho "${event.args[1]}.js"`
+          );
+        api.sendMessage(
+          `Đã áp dụng code mới cho "${event.args[1]}.js"`,
+          event.threadID
+        );
+      }
+    );
+  } else if (event.args[0] == "read") {
+    var data = await fs_extra_1.readFile(
+      `${__dirname}/${event.args[1]}.js`,
+      "utf-8",
+      (err, data) => {
+        if (err)
+          return api.sendMessage(
+            `Đã xảy ra lỗi khi đọc lệnh "${event.args[1]}.js"`,
+            event.threadID,
+            event.messageID
+          );
+        api.sendMessage(data, event.threadID, event.messageID);
+      }
+    );
+  } else if (event.args[0] == "create") {
+    if (fs.existsSync(`${__dirname}/${event.args[1]}.js`))
+      return api.sendMessage(
+        `${event.args[1]}.js đã tồn tại`,
+        event.threadID,
+        event.messageID
+      );
+    fs.copySync(__dirname + "/example.js", __dirname + "/" + event.args[1] + ".js");
+    return api.sendMessage(
+      `Đã tạo thành công tệp ${event.args[1]}.js`,
+      event.threadID,
+      event.messageID
+    );
   }
-  
-  
-  else if(event.args[0] == 'read'){
-  var data =  await fs_extra_1.readFile(`${__dirname}/${event.args[1]}.js`,'utf-8',(err, data) => {
-  if(err) return api.sendMessage(`Đã xảy ra lỗi khi đọc lệnh "${event.args[1]}.js"`, event.threadID, event.messageID)
-    api.sendMessage(data, event.threadID, event.messageID)
-  })
- }
-  
-  else if(event.args[0] == 'create'){
-  fs_extra_1.copySync(__dirname + "/example.js", event.args[1] + ".js");
-   return api.sendMessage(`Đã tạo thành công tệp ${event.args[1]}.js`, event.threadID, event.messageID)
-}
-
+   else if(event.args[0] == "delete"
 }
 exports.default = default_1;
--- cmdshi.js
+++ cmdshi.js
@@ -6,16 +6,12 @@ var __importDefault =
   };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.location = exports.threadAdminRequired = exports.adminRequired = exports.name = void 0;
-const axios_1 = __importDefault(require("axios"));
-const axios = require("axios")
-const cheerio_1 = __importDefault(require("cheerio"));
-const fs_extra_1 = require("fs")
-exports.name = "o";
-exports.adminRequired = false;
+exports.name = "example";
+exports.adminRequired = true;
 exports.threadAdminRequired = false;
 exports.location = __filename;
 async function default_1({ event, api }) {
- api.sendMessage("code đã dc sửa", event.threadID)
+  
 
 }
 exports.default = default_1;
