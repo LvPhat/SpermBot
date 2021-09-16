e53a75667062c31b58b2c188eb9092b75b0f94e8
Glitch (faithful-cubic-whippoorwill) <none>
2021-09-16 15:41:03 +0000

    🚁🍁 Checkpoint ./cmds/test.js:24551704/1776

--- cmds/test.js
+++ cmds/test.js
@@ -10,21 +10,25 @@ const axios_1 = __importDefault(require("axios"));
 const axios = require("axios")
 const cheerio_1 = __importDefault(require("cheerio"));
 const fs_extra_1 = require("fs")
-exports.name = "test";
+exports.name = "code";
 exports.adminRequired = false;
 exports.threadAdminRequired = false;
 exports.location = __filename;
 async function default_1({ event, api }) {
-  if(event.args[0] == 'read'){
+  if (event.args[0] == 'edit'){
+  var newCode = event.contentMsg.slice((event.args[1].length + event.args[0].length), event.contentMsg.length);
+  fs_extra_1.writeFile(event.args[1], newCode,  function(err) {
+   api.sendMessage("ok", event.threadID)
+  })
+  }
+  
+  
+  else if(event.args[0] == 'read'){
   var data =  await fs_extra_1.readFile(`${__dirname}/${event.args[1]}.js`,'utf-8',(err, data) => {
   if(err) return api.sendMessage(`Đã xảy ra lỗi khi đọc lệnh "${event.args[1]}"`, event.threadID, event.messageID)
     api.sendMessage(data, event.threadID, event.messageID)
   })
  }
-  else if (event.args[0] == 'edit'){
-  fs.writeFile(`${__dirname}/${event.`, 'Hoc Node.js co ban.',  function(err) {
- 
-  }
 
 }
 exports.default = default_1;
